65a68c742fb5cde9d6da162f363dec2bce7e032c
Felipe Fadoni <felipe.fadoni@mult-e.com.br>
2020-03-30 18:29:03 -0300

    first commit

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+// import { Container } from './styles';
+
+export default function Incidents() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "blue" }}>
+      <View>
+        <Text>Inciudcents</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
